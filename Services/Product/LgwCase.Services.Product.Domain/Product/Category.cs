048d4f3b8ee7a46dc46dec62b96c6bf453f619c6
ibrahim Yıldız <ibrahim0015@gmail.com>
2021-12-09 11:27:22 +0300

    api ef

---
+++ Services/Product/LgwCase.Services.Product.Domain/Product/Category.cs
@@ -0,0 +1,17 @@
+﻿using LgwCase.Services.Product.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LgwCase.Services.Product.Domain.Product
+{
+    public class Category : Entity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int LimitQuantity { get; set; }
+
+    }
+}
---
+++ Services/Product/LgwCase.Services.Product.Domain/Product/Product.cs
@@ -0,0 +1,30 @@
+﻿using LgwCase.Services.Product.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LgwCase.Services.Product.Domain.Product
+{
+    public class Product : Entity
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        //public int CategoryId { get; private set; }
+        public int StockQuantity { get; private set; }
+
+        public Category Category;
+
+        public Product(int id, string title, string description/*, int categoryId*/, int stockQuantity, Category category)
+        {
+            Id = id;
+            Title = title ?? throw new ArgumentNullException(nameof(title));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            //CategoryId = categoryId;
+            StockQuantity = stockQuantity;
+            Category = category;
+        }
+    }
+}
