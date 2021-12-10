9b836899cc75245bdd504ec2f0c396d46d97f9c6
ibrahim Yıldız <ibrahim0015@gmail.com>
2021-12-10 10:50:48 +0300

    test created

---
+++ Tests/LgwCase.Services.Product.Application.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LgwCase.Services.Product.Application.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
